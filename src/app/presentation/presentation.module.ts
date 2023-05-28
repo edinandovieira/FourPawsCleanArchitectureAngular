3d3e733770015590d8af7bc343cb6a07143bea29
edinando.vieira <edvieira93@gmail.com>
2023-05-28 13:42:00 -0300

    feature: Home and Login pages

---
+++ src/app/presentation/presentation.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ViewModule } from './view/view.module';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ViewModule
+  ]
+})
+export class PresentationModule { }
